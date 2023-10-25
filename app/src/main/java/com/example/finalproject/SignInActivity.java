fcde646bce40b53800833facbfc2419f6ceffcb9
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-10-25 14:30:35 +0700

    update sign in and sign up ui

--- app/src/main/java/com/example/finalproject/SignInActivity.java
+++ app/src/main/java/com/example/finalproject/SignInActivity.java
@@ -2,13 +2,28 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+    TextView tvSignUp;
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        bind();
+        tvSignUp.setOnClickListener(v -> {
+            startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+            finish();
+        });
+    }
+
+    private void bind(){
+        tvSignUp=findViewById(R.id.tvSignIn);
+        btnSignIn=findViewById(R.id.btnSignIn);
     }
 }
--- app/src/main/java/com/example/finalproject/SignUpActivity.java
+++ app/src/main/java/com/example/finalproject/SignUpActivity.java
@@ -2,13 +2,24 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
-
+TextView tvSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        bind();
+        tvSignIn.setOnClickListener(v -> {
+            startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+            finish();
+        });
+    }
+
+    private void bind() {
+        tvSignIn = findViewById(R.id.tvSignIn);
     }
 }
