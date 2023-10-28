c7c442f7c15c594b8a83a29d2c20dccdc9457459
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-10-28 15:48:58 +0700

    update user request param

--- app/src/main/java/com/example/finalproject/model/UserRegistrationRequest.java
+++ app/src/main/java/com/example/finalproject/model/UserRegistrationRequest.java
@@ -3,12 +3,14 @@ package com.example.finalproject.model;
 public class UserRegistrationRequest {
     private String firstName;
     private String lastName;
+    private String roleId;
     private String email;
     private String uuid;
 
-    public UserRegistrationRequest(String firstName, String lastName, String email, String uuid) {
+    public UserRegistrationRequest(String firstName, String lastName,String roleId, String email, String uuid) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.roleId = roleId;
         this.email = email;
         this.uuid = uuid;
     }
